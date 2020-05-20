78a7c59ed15600ddb3ad419f8d85ad2567ba7941
Michael <mikemikhailov2003@gmail.com>
2020-05-20 20:21:52 +0300

    Removed unused validations

--- src/utils/validateInput.js
+++ src/utils/validateInput.js
@@ -1,7 +1,3 @@
-import firebase from './firebaseSetup';
-
-const db = firebase.firestore();
-
 export const isEmail = (email) => {
   const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return email.match(emailRegEx);
@@ -28,59 +24,14 @@ export const validateUserCredentials = (user, signUp) => {
 
 export const validateCreateAppointment = async (appointment) => {
   const errors = {};
-  let barber;
-  if (appointment.barberID) {
-    barber = await db.collection('barbers').doc(appointment.barberID).get();
-    if (!barber.exists) {
-      errors.barber = "The barber with a set id doesn't exist";
-    }
-  }
   if (appointment.email && !isEmail(appointment.email)) {
     errors.email = 'Must be valid email address';
-  }
-  if (appointment.time && Number.isNaN(new Date(appointment.time).getTime())) {
-    errors.time = 'Must be valid time';
   }
   Object.keys(appointment).forEach((key) => {
     if (!appointment[key] || appointment[key].trim() === '') {
       errors[key] = 'Must not be empty';
     }
   });
-  return {
-    errors,
-    valid: Object.keys(errors).length === 0,
-  };
-};
-
-export const validateGetAppointment = async (appointment) => {
-  const errors = {};
-  let barber;
-  if (appointment.barberID) {
-    barber = await db.collection('barbers').doc(appointment.barberID).get();
-    if (!barber.exists) {
-      errors.barber = "The barber with a set id doesn't exist";
-    }
-  }
-  if (appointment.day && Number.isNaN(new Date(appointment.day).getTime())) {
-    errors.day = 'Must be valid time';
-  }
-  if (!appointment.day) {
-    errors.day = 'Must not be empty';
-  }
-  return {
-    errors,
-    valid: Object.keys(errors).length === 0,
-  };
-};
-
-export const validateGetBarberForTime = (time) => {
-  const errors = {};
-  if (time && Number.isNaN(new Date(time).getTime())) {
-    errors.time = 'Must be valid time';
-  }
-  if (!time) {
-    errors.time = 'Must not be empty';
-  }
   return {
     errors,
     valid: Object.keys(errors).length === 0,
